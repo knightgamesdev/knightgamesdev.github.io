2aa2787128158df3bed02acb7ea66a279cb91df5
knightgamesdev <knight.games@yandex.com>
2025-08-01 15:45:19 +0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+
+
+const cdnBase = "https://knightgamesdev.github.io/static/";
+
+const USE_CDN = true; //local -> false, online -> true
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (USE_CDN) {
+      document.querySelectorAll('[src], [href]').forEach(el => {
+        const attr = el.hasAttribute('src') ? 'src' : 'href';
+        const value = el.getAttribute(attr);
+  
+        if (value && value.includes("static/")) {
+          const staticIndex = value.indexOf("static/");
+          const relativePath = value.slice(staticIndex + "static/".length);
+          el.setAttribute(attr, cdnBase + relativePath);
+        }
+      });
+    }
+  });
